79b69386d99428edeb037b1208342637b8131712
cherrycrush <phobosnz@gmail.com>
2022-11-19 11:46:59 +1300

    JSX: Card component

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -8,14 +8,7 @@ function Banner() {
         <img id="logo" src="images/sun-text.svg" alt="Logo"></img>
         <img id="logotext" src="images/logotype-colourful.svg" alt="Logo Text"></img>
       </section>
-      <section className="content">
-        <div className="header">
-          <img id="star" src="images/decoration-pink.svg" alt=""></img>
-          <h3>Our Story</h3>
-          <img id="star" src="images/decoration-orange.svg" 
-          alt=""></img>
-        </div>
-      </section>
+
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,19 @@ import '../App.css';
 
 function Card() {
   return (
-    <p>Our story is reminiscent of the 70's and embody the warmth of the Golden State California.</p>
+        <>
+          <div className="header">
+            <img id="star" src="images/decoration-pink.svg" alt=""></img>
+            <h2>Our Story</h2>
+            <img id="star" src="images/decoration-orange.svg" 
+            alt=""></img>
+          </div>
+          <section className="content">
+          <p>
+            Our story is reminiscent of the 70's and embody the warmth of the Golden State California.
+          </p>
+        </section>
+        </>
   );
 }
 
